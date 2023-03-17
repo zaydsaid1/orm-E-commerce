901d955a866a99c98222426e119d0706f3dd443c
“Zayd <zsaid735@gmail.com>
2023-03-17 03:01:04 +0000

    delete request rectified

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const sequelize = require('./config/connection');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
